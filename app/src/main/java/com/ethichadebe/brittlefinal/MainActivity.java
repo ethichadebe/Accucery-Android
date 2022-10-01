48bf111fbbefa00bd0b267b2bc02763399c5ec89
Ethic Hadebe <ethichadebe@gmail.com>
2022-10-01 20:48:34 +0200

    Setting up foundation(Mobile backend)

--- app/src/main/java/com/ethichadebe/brittlefinal/MainActivity.java
+++ app/src/main/java/com/ethichadebe/brittlefinal/MainActivity.java
@@ -1,14 +1,21 @@
 package com.ethichadebe.brittlefinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.ethichadebe.brittlefinal.viewmodel.ShopViewModel;
+
 public class MainActivity extends AppCompatActivity {
+    ShopViewModel shopViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //RecyclerView rvItems = findViewById(R.id.rvItems);
+
     }
 }

ba81a1bf9e57c55b718916a6fcd7d23e6f070d82
Ethic Hadebe <ethichadebe@gmail.com>
2022-10-01 23:13:59 +0200

    Initial display

--- app/src/main/java/com/ethichadebe/brittlefinal/MainActivity.java
+++ app/src/main/java/com/ethichadebe/brittlefinal/MainActivity.java
@@ -1,11 +1,19 @@
 package com.ethichadebe.brittlefinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.ethichadebe.brittlefinal.adapters.ShopItemAdapter;
+import com.ethichadebe.brittlefinal.local.model.Shop;
 import com.ethichadebe.brittlefinal.viewmodel.ShopViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ShopViewModel shopViewModel;
@@ -15,7 +23,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //RecyclerView rvItems = findViewById(R.id.rvItems);
+        RecyclerView rvItems = findViewById(R.id.rvItems);
+        rvItems.setLayoutManager(new GridLayoutManager(this,2));
+        rvItems.setHasFixedSize(true);
+
+
+        shopViewModel = new ViewModelProvider(this).get(ShopViewModel.class);
+        shopViewModel.getShops().observe(this, new Observer<List<Shop>>() {
+            @Override
+            public void onChanged(List<Shop> shops) {
+                final ShopItemAdapter adapter = new ShopItemAdapter(MainActivity.this,shops);
+                rvItems.setAdapter(adapter);
+
+                Toast.makeText(MainActivity.this, "works", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/ethichadebe/brittlefinal/adapters/ShopItemAdapter.java
@@ -0,0 +1,94 @@
+package com.ethichadebe.brittlefinal.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.ethichadebe.brittlefinal.R;
+import com.ethichadebe.brittlefinal.local.model.Shop;
+
+import java.util.List;
+
+public class ShopItemAdapter extends RecyclerView.Adapter<ShopItemAdapter.ShopItemViewHolder> {
+    private static final String TAG = "ShopItemAdapter";
+    private Context context;
+
+    private List<Shop> shops;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public static class ShopItemViewHolder extends RecyclerView.ViewHolder {
+        public ImageView ivPreview;
+        public TextView tvShopName, tvnItems,tvPrice;
+        public RelativeLayout rlCard;
+
+        public ShopItemViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
+            super(itemView);
+
+            ivPreview = itemView.findViewById(R.id.ivPreview);
+            tvShopName = itemView.findViewById(R.id.tvShopName);
+            tvnItems = itemView.findViewById(R.id.tvnItems);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+            rlCard = itemView.findViewById(R.id.rlCard);
+
+            itemView.setOnClickListener(view -> {
+                if (listener != null) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(position);
+                    }
+                }
+            });
+        }
+    }
+
+    public ShopItemAdapter(Context context, List<Shop> shops) {
+        this.context = context;
+        this.shops = shops;
+    }
+
+    @NonNull
+    @Override
+    public ShopItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.shop_item, parent, false);
+
+        ShopItemViewHolder civh = new ShopItemViewHolder(v, listener);
+
+        return civh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ShopItemViewHolder holder, int position) {
+        Shop item = shops.get(position);
+
+        Glide.with(context).load(item.getImage()).placeholder(R.mipmap.ic_launcher).into(holder.ivPreview);
+        holder.tvShopName.setText(item.getName());
+
+        if (!item.isActive() ){
+            holder.rlCard.setForeground(context.getResources().getDrawable(R.color.opacity));
+        }else {
+            holder.rlCard.setForeground(null);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return shops.size();
+    }
+}

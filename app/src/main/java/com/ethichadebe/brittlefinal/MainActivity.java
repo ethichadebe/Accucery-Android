9fdc50878f4b27374ee402b95fa175a0b9948a23
Ethic Hadebe <ethichadebe@gmail.com>
2022-10-04 22:48:55 +0200

    Initial bottom sheet

--- app/src/main/java/com/ethichadebe/brittlefinal/MainActivity.java
+++ app/src/main/java/com/ethichadebe/brittlefinal/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ethichadebe.brittlefinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -8,11 +10,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.ethichadebe.brittlefinal.adapters.ShopItemAdapter;
 import com.ethichadebe.brittlefinal.local.model.Shop;
 import com.ethichadebe.brittlefinal.viewmodel.ShopViewModel;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,10 +27,17 @@ public class MainActivity extends AppCompatActivity {
     private ShopViewModel shopViewModel;
     private List<Shop> shops = new ArrayList<>();
 
+    private BottomSheetBehavior mBehavior;
+    private View bottomSheet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomSheet = findViewById(R.id.rlBottomSheet);
+
+        mBehavior = BottomSheetBehavior.from(bottomSheet);
+        mBehavior.setPeekHeight(0, true);
 
         RecyclerView rvItems = findViewById(R.id.rvItems);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
@@ -32,14 +45,33 @@ public class MainActivity extends AppCompatActivity {
         final ShopItemAdapter adapter = new ShopItemAdapter();
         rvItems.setAdapter(adapter);
 
+        adapter.setOnItemClickListener(new ShopItemAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                mBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
         shopViewModel = new ViewModelProvider(this).get(ShopViewModel.class);
         shopViewModel.getShops().observe(this, new Observer<List<Shop>>() {
             @Override
             public void onChanged(List<Shop> shops) {
                 adapter.setShopAdapter(MainActivity.this, shops);
-                Toast.makeText(MainActivity.this, "OnChanged ", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "OnChanged ", Toast.LENGTH_SHORT).show();
             }
         });
 
+        final BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_DRAGGING) {
+                    behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+        });
     }
 }
